c83d906200c0a5b6406cf9ea2b8c9a70b4087186
Laura <32573962+ErdaradunGaztea@users.noreply.github.com>
2022-01-04 13:20:55 +0100

    change example to barbell graph

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import networkx as nx
 
 from initialize import ClusterControl, ClusterArray, find_merge, Partition
 
-G = nx.windmill_graph(10, 3)
+# G = nx.windmill_graph(10, 3)
+G = nx.generators.barbell_graph(10, 2)
 
-# plt.figure(num=None, figsize=(30, 30), dpi=80)
-# plt.axis('off')
-# fig = plt.figure(1)
-# nx.draw(G, with_labels=True,node_size=1000)
-# plt.title("Regular Layout")
-# plt.show()
+plt.figure(num=None, figsize=(30, 30), dpi=80)
+plt.axis('off')
+fig = plt.figure(1)
+nx.draw(G, with_labels=True,node_size=1000)
+plt.title("Regular Layout")
+plt.show()
 
 cc = ClusterControl(G, t=500)
 clusters = ClusterArray(G, cc)
